d24b3cec1148ce4b4e7a1859e31b19c2d823be8d
qwertz19281 <dellen903@gmail.com>
2019-12-02 15:09:51 +0100

    option fns

--- src/imp/option.rs
+++ src/imp/option.rs
@@ -8,6 +8,16 @@ pub trait OptionExt<T> {
     #[inline] fn sub_to<V>(&mut self, v: V) where T: SubAssign<V>;
     #[inline] fn mul_to<V>(&mut self, v: V) where T: MulAssign<V>;
     #[inline] fn div_to<V>(&mut self, v: V) where T: DivAssign<V>;
+
+    #[inline] fn add_to_lossy<V>(&mut self, v: Option<V>) where T: AddAssign<V>;
+    #[inline] fn sub_to_lossy<V>(&mut self, v: Option<V>) where T: SubAssign<V>;
+    #[inline] fn mul_to_lossy<V>(&mut self, v: Option<V>) where T: MulAssign<V>;
+    #[inline] fn div_to_lossy<V>(&mut self, v: Option<V>) where T: DivAssign<V>;
+
+    #[inline] fn add_to_if<V>(&mut self, v: Option<V>) where T: AddAssign<V>;
+    #[inline] fn sub_to_if<V>(&mut self, v: Option<V>) where T: SubAssign<V>;
+    #[inline] fn mul_to_if<V>(&mut self, v: Option<V>) where T: MulAssign<V>;
+    #[inline] fn div_to_if<V>(&mut self, v: Option<V>) where T: DivAssign<V>;
 }
 
 impl<T> OptionExt<T> for Option<T> {
@@ -29,5 +39,55 @@ impl<T> OptionExt<T> for Option<T> {
     }
     #[inline] fn div_to<V>(&mut self, v: V) where T: DivAssign<V> {
         self.with_mut(|s| DivAssign::div_assign(s,v) );
+    }
+
+    #[inline] fn add_to_lossy<V>(&mut self, v: Option<V>) where T: AddAssign<V> {
+        if let Some(v) = v {
+            self.add_to(v)
+        }else{
+            *self = None;
+        }
+    }
+    #[inline] fn sub_to_lossy<V>(&mut self, v: Option<V>) where T: SubAssign<V> {
+        if let Some(v) = v {
+            self.sub_to(v)
+        }else{
+            *self = None;
+        }
+    }
+    #[inline] fn mul_to_lossy<V>(&mut self, v: Option<V>) where T: MulAssign<V> {
+        if let Some(v) = v {
+            self.mul_to(v)
+        }else{
+            *self = None;
+        }
+    }
+    #[inline] fn div_to_lossy<V>(&mut self, v: Option<V>) where T: DivAssign<V> {
+        if let Some(v) = v {
+            self.div_to(v)
+        }else{
+            *self = None;
+        }
+    }
+
+    #[inline] fn add_to_if<V>(&mut self, v: Option<V>) where T: AddAssign<V> {
+        if let Some(v) = v {
+            self.add_to(v)
+        }
+    }
+    #[inline] fn sub_to_if<V>(&mut self, v: Option<V>) where T: SubAssign<V> {
+        if let Some(v) = v {
+            self.sub_to(v)
+        }
+    }
+    #[inline] fn mul_to_if<V>(&mut self, v: Option<V>) where T: MulAssign<V> {
+        if let Some(v) = v {
+            self.mul_to(v)
+        }
+    }
+    #[inline] fn div_to_if<V>(&mut self, v: Option<V>) where T: DivAssign<V> {
+        if let Some(v) = v {
+            self.div_to(v)
+        }
     }
 }
