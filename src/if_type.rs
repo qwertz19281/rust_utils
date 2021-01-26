f4eae5b2ffd5501aef1beb3d6a3540be5a908172
qwertz19281 <dellen903@gmail.com>
2021-01-26 22:25:13 +0100

    doc

--- src/if_type.rs
+++ src/if_type.rs
@@ -1,11 +1,12 @@
 use std::any::TypeId;
 
+/// run if types are identical
 #[inline]
 pub fn if_type<Specific: 'static, T: 'static>(f: impl FnOnce() -> Specific) -> Option<T> {
     if TypeId::of::<T>() == TypeId::of::<Specific>() {
         let typed = f();
-        let erased: T = unsafe{ std::mem::transmute_copy(&typed) };
-        std::mem::forget(typed);
+        let erased: T = unsafe{ std::mem::transmute_copy(&typed) }; //type and alignment ensured by identical type
+        std::mem::forget(typed); //else we get some fancy double free
         Some(erased)
     }else{
         None
